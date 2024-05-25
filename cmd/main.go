ef1ce8d8812cf2cec0a8cde7e6d0d89d65445c9b
gauss2302 <shilov6865@gmail.com>
2024-05-25 23:57:49 +0500

    API and user handlers set up

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gauss2302/buymania_backend/cmd/api"
+	"log"
+)
+
+func main() {
+	server := api.NewAPIServer(":8080", nil)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
